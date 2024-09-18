ca9ceab5048f75b23702facd7fbe261d441b6396
Deepak3015 <d03748651@gmail.com>
2024-09-18 14:17:11 +0530

    Intial comment

---
+++ console_full_projects/bin/console_full_projects.dart
@@ -0,0 +1,5 @@
+import 'package:console_full_projects/console_full_projects.dart' as console_full_projects;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_full_projects.calculate()}!');
+}
---
+++ console_full_projects/test/console_full_projects_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_full_projects/console_full_projects.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
